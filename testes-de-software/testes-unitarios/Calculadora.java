ff874025054479db80976009cb2667767bee2a6c
Jean Carlo Galliani <jean.galliani@edu.sc.senai.br>
2023-04-06 11:42:36 -0300

    Create Calculadora.java

---
+++ testes-de-software/testes-unitarios/Calculadora.java
@@ -0,0 +1,21 @@
+public class Calculadora {
+  
+    public double adicionar(double a, double b) {
+        return a + b;
+    }
+
+    public double subtrair(double a, double b) {
+        return a - b;
+    }
+
+    public double multiplicar(double a, double b) {
+        return a * b;
+    }
+
+    public double dividir(double a, double b) {
+        if (b == 0) {
+            throw new IllegalArgumentException("Divisão por zero");
+        }
+        return a / b;
+    }
+}
